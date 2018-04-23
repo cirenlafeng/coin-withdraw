88f3f08f31e697f4ce9b60db0a59a4dc1278e6de
caishiqiang <sean.cai@onemena.com>
2018-04-23 15:20:43 +0800

    更新

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -65,6 +65,7 @@ class HomeController extends Controller
         );
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HEADER, 1);
         curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         $data = curl_exec($curl);
